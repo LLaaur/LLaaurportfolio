37b2c4f8ffc9f745dd2c9e3c533fc529b2363960
Laur <laur_laurentiu2013@yahoo.com>
2023-08-30 15:04:21 +0300

    create file structure for projects section, create projectsIndex

---
+++ portfolio/src/pages/projects/projectsIndex.js
@@ -0,0 +1,44 @@
+export const myProjects = [
+    {
+        name: "Dreamscape",
+        gif: "https://res.cloudinary.com/duij2o8vg/image/upload/v1693396335/Dreamscape_fzzoey.gif",
+        github: "https://github.com/LLaaur/image_generator",
+        link: "https://laurdreamscape.netlify.app/"
+    },
+
+    {
+        name: "Movienator",
+        gif: "https://res.cloudinary.com/duij2o8vg/image/upload/v1693396561/movienator_khsvwi.gif",
+        github: "https://github.com/LLaaur/Movienator",
+        link: "https://movienatorr.netlify.app/"
+    },
+
+    {
+        name: "Musify",
+        gif: "https://res.cloudinary.com/duij2o8vg/image/upload/v1693396657/musify_jbyezb.gif",
+        github: "https://github.com/LLaaur/musify",
+        link: "https://laurmusify.netlify.app/"
+    },
+
+    {
+        name: "Library",
+        gif: "https://res.cloudinary.com/duij2o8vg/image/upload/v1693396792/library_brovkf.gif",
+        github: "https://github.com/LLaaur/library_project",
+        link: "https://llaaur.github.io/library_project/"
+    },
+
+    {
+        name: "To-Do List",
+        gif: "https://res.cloudinary.com/duij2o8vg/image/upload/v1693396879/to-do_moiddn.gif",
+        github: "https://github.com/LLaaur/to_do_website",
+        link: "https://llaaur.github.io/to_do_website/"
+    },
+
+    {
+        name: "Rock, Paper, Scissors",
+        gif: "https://res.cloudinary.com/duij2o8vg/image/upload/v1693396947/rock-paper_foy2wn.gif",
+        github: "https://github.com/LLaaur/rock-paper-scissors",
+        link: "https://llaaur.github.io/rock-paper-scissors/"
+    }
+];
+
