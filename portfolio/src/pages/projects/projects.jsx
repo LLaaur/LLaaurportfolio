8bf28ff1d99359804553590bb5667d7ef14a2bb6
Laur <laur_laurentiu2013@yahoo.com>
2023-09-04 18:21:55 +0300

    fix projects carousel name

--- portfolio/src/pages/projects/projects.jsx
+++ portfolio/src/pages/projects/projects.jsx
@@ -1,177 +1,177 @@
-import React, {useState} from "react";
-import styles from "./projects.module.css";
-import carouselLight from "../../assets/carousel-light.svg";
-import carouselDark from "../../assets/carousel-dark.svg";
-import {myProjects} from "./projectsIndex";
+import React, { useState } from 'react';
 
+import styles from './projects.module.css';
+import arrowLight from '../../assets/light-arrow.svg';
+import arrowDark from '../../assets/dark-arrow.svg';
+import carouselArrowLight from '../../assets/carousel-light.svg';
+import carouselArrowDark from '../../assets/carousel-dark.svg';
+import { myProjects } from './projectsIndex';
 
 const Projects = (props) => {
+  const [earlierProject, setEarlierProject] = useState(
+    myProjects[myProjects.length - 1]
+  );
+  const [previousProject, setPreviousProject] = useState(myProjects[0]);
+  const [currentProject, setCurrentProject] = useState(myProjects[1]);
+  const [nextProject, setNextProject] = useState(myProjects[2]);
+  const [laterProject, setLaterProject] = useState(myProjects[3]);
 
-    const [earlierProject, setEarlierProject] = useState(myProjects[myProjects.length - 1]);
-    const [previousProject, setPreviousProject] = useState(myProjects[1]);
-    const [currentProject, setCurrentProject] = useState(myProjects[1]);
-    const [nextProject, setNextProject] = useState(myProjects[1]);
-    const [laterProject, setLaterProject] = useState(myProjects[2]);
+  const [goBackButton, setGoBackButton] = useState(0);
+  const [goForwardButton, setGoForwardButton] = useState(0);
 
-    const [goBackBtn, setGoBackBtn] = useState(0);
-    const[goForwardBtn, setGoForwardBtn] = useState(0);
+  let projectName = currentProject.name;
+  let githubLink = currentProject.github;
+  let pageLink = currentProject.link;
 
-    let projectName = currentProject.name;
-    let githubLink = currentProject.github;
-    let websiteLink = currentProject.link;
+  const arrow = props.mode === 'light' ? arrowLight : arrowDark;
+  const carouselArrow =
+    props.mode === 'light' ? carouselArrowLight : carouselArrowDark;
 
-    const carouselArrow = props.mode === "light" ? carouselLight : carouselDark;
+  const onPreviousHandler = () => {
+    setGoBackButton(1);
+    setTimeout(() => {
+      const prev = { ...previousProject };
+      const curr = { ...currentProject };
+      const next = { ...nextProject };
+      const latr = { ...laterProject };
+      setEarlierProject(prev);
+      setPreviousProject(curr);
+      setCurrentProject(next);
+      setNextProject(latr);
+      const i = myProjects.findIndex((element) => element.name === latr.name);
+      let newLater;
+      if (i === myProjects.length - 1) {
+        newLater = myProjects[0];
+      } else {
+        newLater = myProjects[i + 1];
+      }
+      setLaterProject(newLater);
+    }, 580);
+  };
 
-    const onPreviousHandler = () => {
+  const onNextHandler = () => {
+    setGoForwardButton(1);
+    setTimeout(() => {
+      const next = { ...nextProject };
+      const curr = { ...currentProject };
+      const prev = { ...previousProject };
+      const earl = { ...earlierProject };
+      setLaterProject(next);
+      setNextProject(curr);
+      setCurrentProject(prev);
+      setPreviousProject(earl);
+      const i = myProjects.findIndex((element) => element.name === earl.name);
+      let newEarlier;
+      if (i === 0) {
+        newEarlier = myProjects[myProjects.length - 1];
+      } else {
+        newEarlier = myProjects[i - 1];
+      }
+      setEarlierProject(newEarlier);
+    }, 580);
+  };
 
-        setGoBackBtn(1);
-        setTimeout(() => {
-            const prev = {...previousProject};
-            const curr = {...currentProject};
-            const next = {...nextProject};
-            const latr = {...laterProject};
+  const config = {
+    delta: 10,
+    preventDefaultTouchmoveEvent: false,
+    trackTouch: true,
+    trackMouse: true,
+    rotationAngle: 0,
+  };
 
-            setEarlierProject(prev);
-            setPreviousProject(curr);
-            setCurrentProject(next);
-            setNextProject(latr);
-
-            const i = myProjects.findIndex((project) => project.name === latr.name);
-            let newLater;
-            if (i === myProjects.length - 1){
-                newLater = myProjects[0]
-            }
-            else{
-                newLater = myProjects[i + 1];
-            }
-            setLaterProject(newLater);
-        }, 580);
-
-    };
-
-    const onNextHandler = () => {
-
-        setGoForwardBtn(1);
-        setTimeout(() => {
-            const next = {...nextProject};
-            const curr = {...currentProject};
-            const prev = {...previousProject};
-            const earl = {...earlierProject};
-
-            setLaterProject(next);
-            setNextProject(curr);
-            setCurrentProject(prev);
-            setPreviousProject(earl);
-
-            const i = myProjects.findIndex((project) => project.name === earl.name);
-            let newEarlier;
-            if (i === 0){
-                newEarlier = myProjects[myProjects.length - 1];
-            }
-            else {
-                newEarlier = myProjects[i - 1];
-            }
-            setEarlierProject(newEarlier);
-        }, 580)
-    };
-
-    return (
-        <div id='projects' className={styles.ProjectsComponent}>
-
-            <h2 mode={props.mode} className={styles.Projects + ' text'}>
-                Projects
-            </h2>
-
-            <div className={styles.CarouselContainer}>
-                <div className={styles.ProjGifs}>
-                    <img
-                        goforward={goForwardBtn}
-                        goback={goBackBtn}
-                        onAnimationEnd={() => {
-                            setGoBackBtn(0);
-                            setGoForwardBtn(0);
-                        }}
-                        src={earlierProject.gif}
-                        className={styles.EarlierProj}
-                        alt=''
-                    />
-                    <img
-                        goforward={goForwardBtn}
-                        goback={goBackBtn}
-                        onAnimationEnd={() => {
-                            setGoBackBtn(0);
-                            setGoForwardBtn(0);
-                        }}
-                        src={previousProject.gif}
-                        className={styles.PrevProj}
-                        alt=''
-                    />
-                    <img
-                        goforward={goForwardBtn}
-                        goback={goBackBtn}
-                        onAnimationEnd={() => {
-                            setGoBackBtn(0);
-                            setGoForwardBtn(0);
-                        }}
-                        src={nextProject.gif}
-                        className={styles.NextProj}
-                        alt=''
-                    />
-                    <img
-                        goforward={goForwardBtn}
-                        goback={goBackBtn}
-                        onAnimationEnd={() => {
-                            setGoBackBtn(0);
-                            setGoForwardBtn(0);
-                        }}
-                        src={laterProject.gif}
-                        className={styles.LaterProj}
-                        alt=''
-                    />
-                </div>
-                <div className={styles.NameContainer}>
-                    <img
-                        onClick={onPreviousHandler}
-                        className={styles.PrevArrow}
-                        src={carouselArrow}
-                        onAnimationEnd={() => setGoBackBtn(0)}
-                        alt=''
-                    />
-                    <p className={styles.ProjectName}>{projectName}</p>
-                    <img
-                        onClick={onNextHandler}
-                        className={styles.NextArrow}
-                        src={carouselArrow}
-                        alt=''
-                        onAnimationEnd={() => setGoForwardBtn(0)}
-                    />
-                </div>
-                <div className={styles.LinksContainer}>
-                    <a
-                        mode={props.mode}
-                        target='_blank'
-                        rel='noopener noreferrer'
-                        className={styles.Link + ' linkText linkBackground'}
-                        href={githubLink}
-                    >
-                        See Github
-                    </a>
-                    <a
-                        mode={props.mode}
-                        target='_blank'
-                        rel='noopener noreferrer'
-                        className={styles.Link + ' linkText linkBackground'}
-                        href={websiteLink}
-                    >
-                        See Project
-                    </a>
-                </div>
-            </div>
-
-
+  return (
+    <div id='projects' className={styles.ProjectsComponent}>
+      <h2 mode={props.mode} className={styles.Projects + ' text'}>
+        Projects
+      </h2>
+      <div className={styles.CarouselContainer}>
+        <div className={styles.ProjGifs}>
+          <img
+            goforward={goForwardButton}
+            goback={goBackButton}
+            onAnimationEnd={() => {
+              setGoBackButton(0);
+              setGoForwardButton(0);
+            }}
+            src={earlierProject.gif}
+            className={styles.EarlierProj}
+            alt=''
+          />
+          <img
+            goforward={goForwardButton}
+            goback={goBackButton}
+            onAnimationEnd={() => {
+              setGoBackButton(0);
+              setGoForwardButton(0);
+            }}
+            src={previousProject.gif}
+            className={styles.PrevProj}
+            alt=''
+          />
+          <img
+            goforward={goForwardButton}
+            goback={goBackButton}
+            onAnimationEnd={() => {
+              setGoBackButton(0);
+              setGoForwardButton(0);
+            }}
+            src={nextProject.gif}
+            className={styles.NextProj}
+            alt=''
+          />
+          <img
+            goforward={goForwardButton}
+            goback={goBackButton}
+            onAnimationEnd={() => {
+              setGoBackButton(0);
+              setGoForwardButton(0);
+            }}
+            src={laterProject.gif}
+            className={styles.LaterProj}
+            alt=''
+          />
         </div>
-    );
-
-}
+        <div className={styles.NameContainer}>
+          <img
+            onClick={onPreviousHandler}
+            className={styles.PrevArrow}
+            src={carouselArrow}
+            onAnimationEnd={() => setGoBackButton(0)}
+            alt=''
+          />
+          <p className={styles.ProjectName}>{projectName}</p>
+          <img
+            onClick={onNextHandler}
+            className={styles.NextArrow}
+            src={carouselArrow}
+            alt=''
+            onAnimationEnd={() => setGoForwardButton(0)}
+          />
+        </div>
+        <div className={styles.LinksContainer}>
+          <a
+            mode={props.mode}
+            target='_blank'
+            rel='noopener noreferrer'
+            className={styles.Link + ' linkText linkBackground'}
+            href={githubLink}
+          >
+            See Github
+          </a>
+          <a
+            mode={props.mode}
+            target='_blank'
+            rel='noopener noreferrer'
+            className={styles.Link + ' linkText linkBackground'}
+            href={pageLink}
+          >
+            See Project
+          </a>
+        </div>
+      </div>
+      <img className={styles.Arrow} src={arrow} alt='' />
+    </div>
+  );
+};
 
 export default Projects;
--- portfolio/src/pages/projects/projectsIndex.js
+++ portfolio/src/pages/projects/projectsIndex.js
@@ -1,38 +1,43 @@
 export const myProjects = [
     {
-        name: "Dreamscape",
         gif: "https://res.cloudinary.com/duij2o8vg/image/upload/v1693396335/Dreamscape_fzzoey.gif",
         github: "https://github.com/LLaaur/image_generator",
-        link: "https://laurdreamscape.netlify.app/"
+        link: "https://laurdreamscape.netlify.app/",
+        name: "Musify"
     },
     {
-        name: "Movienator",
+
         gif: "https://res.cloudinary.com/duij2o8vg/image/upload/v1693396561/movienator_khsvwi.gif",
         github: "https://github.com/LLaaur/Movienator",
-        link: "https://movienatorr.netlify.app/"
+        link: "https://movienatorr.netlify.app/",
+        name: "Library"
     },
     {
-        name: "Musify",
+
         gif: "https://res.cloudinary.com/duij2o8vg/image/upload/v1693396657/musify_jbyezb.gif",
         github: "https://github.com/LLaaur/musify",
-        link: "https://laurmusify.netlify.app/"
+        link: "https://laurmusify.netlify.app/",
+        name: "Rock-Paper-Scissors"
     },
     {
-        name: "Library",
+    
         gif: "https://res.cloudinary.com/duij2o8vg/image/upload/v1693396792/library_brovkf.gif",
         github: "https://github.com/LLaaur/library_project",
-        link: "https://llaaur.github.io/library_project/"
+        link: "https://llaaur.github.io/library_project/",
+        name: "To-Do List"
     },
     {
-        name: "Rock-Paper-Scissors",
+       
         gif: "https://res.cloudinary.com/duij2o8vg/image/upload/v1693396947/rock-paper_foy2wn.gif",
         github: "https://github.com/LLaaur/rock-paper-scissors",
         link: "https://llaaur.github.io/rock-paper-scissors/",
+        name: "Dreamscape"
     },
     {
-        name: "To-Do List",
+       
         gif: "https://res.cloudinary.com/duij2o8vg/image/upload/v1693396879/to-do_moiddn.gif",
         github: "https://github.com/LLaaur/to_do_website",
-        link: "https://llaaur.github.io/to_do_website/"
-    }
-]
+        link: "https://llaaur.github.io/to_do_website/",
+        name: "Movienator"
+    },
+];
